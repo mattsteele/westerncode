2a7af48a8032ae06332025c6eed611a10e20fe32
Matt Steele <mattsteele000@gmail.com>
2025-04-18 17:13:01 -0600

    Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+  compressHTML: true,
+  site: 'https://www.westerncode.com',
+});
